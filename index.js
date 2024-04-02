7315662023c8eafe34fde1ab2866912be07afb1b
Javokhir1105 <161455530+Javokhir1105@users.noreply.github.com>
2024-04-02 19:35:58 +0500

    js misollar

---
+++ index.js
@@ -0,0 +1,101 @@
+// let x=prompt("age kiriting)
+// if(x<20){
+//     console.log("Hush kelibsan")
+// }
+// else{
+//     console.log("hali yoshsan")
+// }
+
+let b=prompt("bni kiriting")
+let a=prompt("ani kiriting")
+let c=prompt("cni kiriting")
+if(a+b>c && a+c>b && b+c>a){
+    console.log("uchburchak bo'ladi")
+}
+else{
+    console.log("uchburchak emas")
+}
+
+
+// let x=prompt("xni kiriting")
+// let y=prompt("yni kiriting")
+// let c=prompt("cni kiriting")
+//  if(c=*){
+//     console.log(x*y)
+//  }
+//  else{
+//     console.log(x+y)
+//  }
+
+// let x=prompt("ani kiriting")
+// if(x>0){
+//     console.log(Math.sqrt(x))
+// }
+// else(
+//     console.log("manfiy")
+// }
+
+
+// let x=prompt("ani kiriting")
+// if(x>0 && x==a*a && a>0){
+//     console.log(Math.sqrt(x))
+// }
+// else(
+//     console.log("manfiy")
+// )
+
+
+// let x=prompt("yilni kiriting")
+// if(x%4==0){
+//     console.log("bu kabisa yili")
+// }
+// else{
+//     console.log("kabisa yili emas")
+// }
+
+// let x=prompt("sonni kiriting")
+// if(x%3==0 && x%5!=0){
+//     console.log("Fizz")
+// }
+// else if(x%5==0 && x%3!=0){
+//     console.log("Buzz")
+// }
+// else if(x%3==0 && x%5==0){
+//     console.log("FizzBuzz")
+// }
+// else{
+//     console.log("adashdingiz")
+// }
+
+
+// let x=prompt("sonni kiriting")
+// let y=prompt("sonni kiriting")
+// if(x%y==0 ){
+//     console.log("x yga bo'linadi")
+// }
+// else if(y%x==0){
+//     console.log("y xga bo'linadi")
+// }
+// else{
+//     console.log("ular o'zaro tub sonlardir")
+// }
+
+// let x=prompt("sonni kiriting")
+// if(x%2==0){
+//     console.log("juft son")
+// }
+// else{
+//     console.log("siz toq son kiritdingiz")
+// }
+
+
+// let x=2
+// let y=5
+// let z=8
+// let S=x+y+z
+// if(S%3==0){
+//     console.log(S/3)
+// }
+// else{
+//     console.log(Math.floor(S/3))
+// }
